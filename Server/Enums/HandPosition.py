3db538f1c691c76c042e76c560b51815d85a560f
Danila Solovenko <dansil2301@gmail.com>
2026-04-10 01:05:36 +0200

    feat: seperate to BE and FE

--- Server/Enums/HandPosition.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class HandPosition(Enum):
-    UP = 'up'
-    DOWN = 'down'
-    RIGHT = 'right'
-    LEFT = 'left'
-    NULL = 'undetectable'
--- Server/Enums/RunningMode.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-from mediapipe.tasks.python import vision
-
-
-class RunningMode(Enum):
-    VIDEO = vision.RunningMode.VIDEO
-    IMG   = vision.RunningMode.IMAGE
-    LIVE  = vision.RunningMode.LIVE_STREAM
