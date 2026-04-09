4e0a8d4cb51492bd4a5d34da03baff1df0065be7
Danila Solovenko <dansil2301@gmail.com>
2026-04-10 01:09:01 +0200

    mend

---
+++ Backend/Server/Enums/HandPosition.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class HandPosition(Enum):
+    UP = 'up'
+    DOWN = 'down'
+    RIGHT = 'right'
+    LEFT = 'left'
+    NULL = 'undetectable'
---
+++ Backend/rewritten_test.py
@@ -0,0 +1,31 @@
+import os
+
+import cv2
+
+from Server.Enums.RunningMode import RunningMode
+from Server.HandDetection import HandDetection
+
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Camera failed to open")
+    exit()
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "hand_landmarker.task")
+hand_detector = HandDetection(MODEL_PATH, RunningMode.VIDEO, 1)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        continue
+    
+    frame = hand_detector.find_hand(frame)
+
+    cv2.imshow("Camera Feed", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
