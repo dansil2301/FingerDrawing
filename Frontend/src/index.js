1d1dcc7f589332b12a05fd6e1db1a793ff7ca8ff
Danila Solovenko <dansil2301@gmail.com>
2026-04-11 00:39:32 +0200

    feat: basic FE is done. Added web sockets to BE

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Canvas from './Canvas';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Canvas />
   </React.StrictMode>
 );
