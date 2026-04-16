e86f5fab51b4c65f77c018ef077a4880c21fe3e5
Danila Solovenko <dansil2301@gmail.com>
2026-04-16 22:02:01 +0200

    fix: add index.js

---
+++ Frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Canvas from './Presentation/Canvas';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Canvas />
+  </React.StrictMode>
+);
